5478d13dfb0f9eab6a3964d802e0fab14ca2c2bf
mirkobrombin <send@mirko.pm>
2024-10-09 23:59:42 +0200

    nvim: add colorschemes

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,18 @@
+return {
+  { "catppuccin/nvim" },
+  { "sainnhe/sonokai" },
+  { "polirritmico/monokai-nightasty.nvim" },
+  { "loctvl842/monokai-pro.nvim" },
+  { "folke/tokyonight.nvim" },
+  { "embark-theme/vim" },
+  { "ellisonleao/gruvbox.nvim" },
+  { "folke/tokyonight.nvim" },
+
+  -- Configure LazyVim to load colorscheme
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight",
+    },
+  },
+}
